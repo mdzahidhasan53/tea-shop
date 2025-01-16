fd8adcc2e722a4d19409270015690519ae632d94
mdzahidhasan53 <mzhasan53@gmail.com>
2025-01-16 19:08:25 +0600

    added featured products

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
       extend: {},
     },
     plugins: [],
